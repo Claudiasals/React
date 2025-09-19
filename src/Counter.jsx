fb222d9d1dc2bbe1e3494a38c2707cf620f033f0
Claudiasals <salsiniclaudia@gmail.com>
2025-09-19 19:49:33 +0200

    Aggiunto bottone per incrementare di 1 il contatore

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,19 +1,31 @@
 import { useState } from "react"
 
-function Counter() {
+const Counter = () => {
     const [count, setCount] = useState(0);
-    /* useState restituisce un array con in posizione 0 il valore dello state
+    /* Destracturing da useState: useState restituisce un array con in posizione 0 il valore dello state
      e in posizione 1 la funzione che va a triggerare il set del valore 
      di state, quindi va a modificare il valore di state andando a 
-     rendirizzare ciò che viene restituito da questo componente */
-     // possiamo inserire più state , basta che abbiano un nome diverso 
-     // es.: const [value, setValue] = useState(0);
-    return (
-        <div>
-        <button></button>
-        <p> Count: {count} </p>
-        </div>
-    )
+     rendirizzare ciò che viene restituito da questo componente. */
+    // Il secondo parametro inizia sempre con "set" e poi il nome della variabile.
+    // possiamo inserire più state , basta che abbiano un nome diverso 
+    // es.: const [value, setValue] = useState(0);
 
-}
-export default Counter
+    //inseriamo nel bottone l'attributo "onClick" con all'interno la funzione {} 
+    // che vogliamo sia eseguita al click, ovvero {handleClickButton}
+    const handleClickButton = () => {
+        setCount((_count) => {// CALLBACK PER accedere al valore precedente per sommare 1
+            //aggiungiamo _ per far capire che sono diversi i cunt ma si chiamano uguae perché sono collegati
+            return _count + 1; // la callback ci deve restituire questo valore 
+        });
+    }
+
+        return (
+            <div>
+                <button onClick={handleClickButton}>ADD 1</button>
+
+                <p> Count: {count} </p>
+            </div>
+        )
+    };
+   
+    export default Counter
