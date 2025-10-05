d19f2087f091c1f491f91a7bb419eaab7c421774
Claudiasals <salsiniclaudia@gmail.com>
2025-10-06 01:49:04 +0200

    comments es.22

--- src/ToDoList2.jsx
+++ src/ToDoList2.jsx
@@ -10,19 +10,26 @@ e un messaggio di errore se qualcosa va storto. */
 import useFetch from "./useFetch";
 
 const ToDoList2 = () => {
-    const { data, loading, error } = useFetch("https://jsonplaceholder.typicode.com/todos");
+// Chiamo l'hook useFetch per recuperare i dati dall'API.
+// L'hook restituisce tre valori: 
+// data = i dati ricevuti dall'API
+// loading = true se la fetch è in corso, false se terminata
+// error = eventuale messaggio di errore se la fetch fallisce    
+const { data, loading, error } = useFetch("https://jsonplaceholder.typicode.com/todos");
     console.log(loading)
     console.log(error)
     console.log(data)
 
+    // mostro i msg di loading e di errore, perché dal useFetch 
+    // prende soltanto i valori e non i msg html
     if (loading) return <p>Caricamento lista To-Do...</p>;
- 
     if (error) return <p>Errore: {error}</p>;
 
 
-    return (
+    return ( // utilizzo map per ciclare e poter utilizzare i dati che mi servono 
+        // da ogni elemento dell'array scaricato
         <>
-        <ul>
+        <ul> 
          {data.map((todo) => (  
           <li key={todo.id}>{todo.title}</li>
          ))}
