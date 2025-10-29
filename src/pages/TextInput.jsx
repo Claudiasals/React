e1e0074864d9ff610f9372b3158ecdac34f1f725
Claudiasals <salsiniclaudia@gmail.com>
2025-10-29 14:28:03 +0100

    integro useRef e useEffect per mettere in focus l'input al montaggio del comonente

--- src/pages/TextInput.jsx
+++ src/pages/TextInput.jsx
@@ -1,29 +1,40 @@
 import { useState } from "react";
+import { useRef, useEffect } from "react"; 
+// importo useRef per mettere il focus sull'input 
+// e importo useEffect per dire a react quando eseguirlo
 
 const TextInput = () => {
-    const [text, setText] = useState(""); // inizializzo stringa vuota
+  // creo la costante per mettere il focus sull'input
+  const inputRef = useRef(null);
 
-    const handleInput = (event) => {
-        setText(event.target.value); // aggiorno lo stato con quello che scrivi
-        console.log(event.target.value); // stampo in console il valore
-      };
-/*
-text → è la variabile di stato che contiene il valore attuale (inizialmente "", 
-cioè stringa vuota).
+  useEffect(() => {
+    inputRef.current.focus(); // metti il cursore quando monta il componente
+  }, []); // dipendenza []: useEffect con array vuoto [] fa sì che il focus venga 
+  // impostato solo al montaggio del componente.
 
-setText → è la funzione che modifica quello stato. Non puoi cambiare text direttamente, 
-devi sempre passare da setText. 
-*/
+  const [text, setText] = useState(""); // inizializzo stringa vuota
 
-/*
----- Cosa succede in setText(event.target.value):
-Quando scrivi dentro un input (<input type="text" />), React ti dà accesso all’evento event:
-event.target → è l’elemento HTML che ha scatenato l’evento, in questo caso l’<input>.
-event.target.value → è il valore attuale dentro la casella di testo (quello che stai digitando).
-*/
+  const handleInput = (event) => {
+    setText(event.target.value); // aggiorno lo stato con quello che scrivi
+    console.log(event.target.value); // stampo in console il valore
+  };
+  /*
+  text → è la variabile di stato che contiene il valore attuale (inizialmente "", 
+  cioè stringa vuota).
+  
+  setText → è la funzione che modifica quello stato. Non puoi cambiare text direttamente, 
+  devi sempre passare da setText. 
+  */
+
+  /*
+  ---- Cosa succede in setText(event.target.value):
+  Quando scrivi dentro un input (<input type="text" />), React ti dà accesso all’evento event:
+  event.target → è l’elemento HTML che ha scatenato l’evento, in questo caso l’<input>.
+  event.target.value → è il valore attuale dentro la casella di testo (quello che stai digitando).
+  */
   return (
     <>
-      <input type="text" value={text} onChange = {handleInput} /> 
+      <input ref={inputRef} type="text" value={text} onChange={handleInput} />
       <p>Hai scritto: {text}</p>
     </>
   )
@@ -33,4 +44,16 @@ event.target.value → è il valore attuale dentro la casella di testo (quello c
 //questo trasforma l’input in un controlled component → l’input mostra sempre il valore che hai nello stato text.
 
 // <p>Hai scritto: {text}</p> Per vedere subito a schermo quello che digitiamo
-  export default TextInput
+export default TextInput
+
+
+/* esercizio :
+React
+
+Gestisci il focus dell'input di ricerca con useRef
+Utilizza useRef per gestire il focus dell'input di ricerca quando il componente viene montato. 
+
+--> Quando un campo di input è “in focus”, 
+significa che il cursore lampeggia dentro di lui, pronto per scrivere.
+
+*/
