e3c700e6a09b80a202a1f88a69f5f1a7952252c8
Claudiasals <salsiniclaudia@gmail.com>
2025-11-04 14:22:54 +0100

    es.outlet: creato componente layout e inserito componente Outlet nel return per gestire il rendering con le parti comuni.

---
+++ src/components/Layout.jsx
@@ -0,0 +1,34 @@
+/*
+Utilizza Outlet per layout comuni:
+Modifica l'applicazione per utilizzare un layout comune con Outlet 
+per il contenuto principale. 
+Crea un componente Layout che includa la barra di navigazione e utilizzi 
+Outlet per il rendering delle pagine.
+*/
+
+/*
+Cos’è Outlet?
+Outlet è un componente fornito da react-router-dom (dalla versione 6 in poi).
+Serve come “punto di uscita” (da qui il nome Outlet) dove verranno renderizzate 
+le pagine figlie di un certo percorso.
+
+Immaginalo così:
+- Layout è il contenitore comune (con Navbar, Footer, ecc.);
+- Outlet è il punto dove React Router “inserisce” il contenuto della pagina attuale. 
+*/
+
+import { Outlet } from "react-router-dom"
+import Navbar from "./Navbar"
+
+const Layout = () => {
+    return (
+        <>
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
+    </>
+    );
+};
+
+export default Layout
