6bd7dab8f94a02e93c81d25327403e87999f0e44
Claudiasals <salsiniclaudia@gmail.com>
2025-11-04 14:49:25 +0100

    es: inserisco bottone home collegato con useNavigate: - importo useNavigate - crea costante per inizializzare e poter chiamare l'hook useNavigate - creo costante per chiamare useNavigate che uso per creare il collegamento con Home. - creo bottone "home" con onclick collegato alla costante che utilizza useNavigate e che quindi ci da le info per la navigazione

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,24 @@
+// importo l'hook useNavigate di react-router-dom 
+// ci permette di navigare senza link
+import { useNavigate } from "react-router-dom";
 
 const About = () => {
+    // inizializzo l'hoook:
+    // creao la costante per chiamare e poter poi
+    // utilizzare l'hook useNavigate
+    const navigate = useNavigate(); 
+
+    // utilizzo useNavigate per creare il collegamento con Home 
+    const goHome = () => { 
+        navigate("/"); // gli dico: naviga verso la Home
+      };
+
     return (
         <>
             <h1>About This App</h1>
             <p>Questa è una semplice applicazione per gestire le tue attività quotidiane.</p>
+            <button onClick={goHome}> HOME </button>
+            {/* in button collego il click alla const che mi da le indicazioni sulla navigazione */}
         </>
     )
 };
